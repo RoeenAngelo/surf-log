4758bdc888dc2deb4de440888c452a8ad42444c4
roeenangelo <roeenangelo@gmail.com>
2026-04-27 18:25:48 -0700

    Add __str__ methods to Spot and Session classes

--- session.py
+++ session.py
@@ -1,11 +1,17 @@
 class Session:
-    def __init__(self, date, wave_height, wind, tide, crowd, uv, board, fins, rating):  
+    def __init__(self, date, time, wave_height, wind, tide, crowd, uv, board, fins, duration, rating, notes):  
         self.date = date
+        self.time = time
         self.wave_height = wave_height
         self.wind = wind
         self.tide = tide
         self.crowd = crowd
-        self.uv
-        self.board
-        self.fins
-        self.rating
+        self.uv = uv
+        self.board = board
+        self.fins = fins
+        self.duration = duration
+        self.rating = rating
+        self.notes = notes
+
+    def __str__(self):
+        return f"Date: {self.date} | Time: {self.time} | Wave_height: {self.wave_height} | Wind: {self.wind} | Tide: {self.tide} | Crowd: {self.crowd} | UV: {self.uv} | Board: {self.board} | Fins: {self.fins} | Duration: {self.duration} | Rating: {self.rating} | Notes: {self.notes}"
--- spot.py
+++ spot.py
@@ -3,3 +3,6 @@ class Spot:
         self.name = name
         self.location = location
         self.difficulty = difficulty
+
+    def __str__(self):
+        return f"Spot: {self.name} | Location: {self.location} | Difficulty: {self.difficulty}"
