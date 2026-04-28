03c60cc231ddca00f7c1dfb5e274d5073ee6f071
roeenangelo <roeenangelo@gmail.com>
2026-04-27 17:51:55 -0700

    Add Spot and Session classes

---
+++ session.py
@@ -0,0 +1,11 @@
+class Session:
+    def __init__(self, date, wave_height, wind, tide, crowd, uv, board, fins, rating):  
+        self.date = date
+        self.wave_height = wave_height
+        self.wind = wind
+        self.tide = tide
+        self.crowd = crowd
+        self.uv
+        self.board
+        self.fins
+        self.rating
---
+++ spot.py
@@ -0,0 +1,5 @@
+class Spot:
+    def __init__(self, name, location, difficulty):
+        self.name = name
+        self.location = location
+        self.difficulty = difficulty
